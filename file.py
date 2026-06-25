b4755fb71a1656e8e14c872503ba021db447ab15
lnjoroge-alu <l.njoroge@alustudent.com>
2026-06-25 06:35:57 +0300

    feat: add main calc functions

--- file.py
+++ file.py
@@ -3,3 +3,15 @@ from urllib.parse import urlparse, parse_qs
 import json
  
 def calculate(op, a, b):
+    if op == 'add':
+        return a + b
+    elif op == 'subtract':
+        return a - b
+    elif op == 'multiply':
+        return a * b
+    elif op == 'divide':
+        if b != 0:
+            return a / b
+        else:
+            raise ValueError("Cannot divide by zero")
+   
