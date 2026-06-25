a78675c4a1f1a61822f0d4d7d7d438296eab6af6
lnjoroge-alu <l.njoroge@alustudent.com>
2026-06-25 06:42:19 +0300

    feat: serve api endpoint

--- file.py
+++ file.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from logging import Handler
 from urllib.parse import urlparse, parse_qs
 import json
  
@@ -16,3 +17,22 @@ def calculate(op, a, b):
             raise ValueError("Cannot divide by zero")
     else:
         raise ValueError("Invalid operation")
+    
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        url = urlparse(self.path)
+        op = url.path.strip("/")
+        params = parse_qs(url.query)
+        a = int(params["a"][0])
+        b = int(params["b"][0])
+ 
+        result = calculate(op, a, b)
+ 
+        body = json.dumps({"a": a, "b": b, "operation": op, "result": result})
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(body.encode())
+
+
+HTTPServer(("localhost", 5000), Handler).serve_forever()
